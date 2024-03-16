68d1a950d80298c4d3910dec0f83dc092accbd45
msuleman007 <143287993+msuleman007@users.noreply.github.com>
2024-03-16 16:21:33 +0500

    Time: 2 ms (56.28%) | Memory: 7 MB (84.37%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.cpp
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        for (long i = 0; i <= num ;i++){
-            if ((i*i)==num){
+        long l = 1;
+        long r = num;
+        while(l<=r){
+            long long mid = (l+r)/2;
+            if (mid*mid > num){
+                r = mid - 1;
+            } else if ( mid * mid < num){
+                l = mid + 1;
+            } else {
                 return true;
             }
         }
