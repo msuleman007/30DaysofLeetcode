b38bc0d9322274388656be3641a20a862454c9c7
Muhammad Suleman <sp23-bcs-064@isbstudent.comsats.edu.pk>
2024-03-28 02:15:13 +0500

    Time: 0 ms (100.00%) | Memory: 8 MB (21.17%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack <char> st;
+        stack<char> st;
         for (char c : s) {
-            if (c == '(')
-                st.push(')');
-            else if (c == '{')
-                st.push('}');
-            else if (c == '[')
-                st.push(']');
-            else {
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c == '(' ? ')' : (c == '{' ? '}' : ']'));
+            } else if (c == ')' || c == '}' || c == ']') {
                 if (st.empty() || st.top() != c)
                     return false;
                 st.pop();
