5bcd904919679b4fbb8d0eb7d7a62a9b44f1cd19
Muhammad Suleman <sp23-bcs-064@isbstudent.comsats.edu.pk>
2024-03-27 21:26:50 +0500

    Time: 0 ms (100.00%) | Memory: 8.1 MB (53.49%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -3,28 +3,25 @@ public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         vector<int> res;
 
-        if (matrix.empty()) // Check if the matrix is empty
+        if (matrix.empty()) 
             return res;
 
         int rowBegin = 0;
         int rowEnd = matrix.size() - 1;
         int colBegin = 0;
-        int colEnd = matrix[0].size() - 1; // Corrected from matrix.size() - 1
+        int colEnd = matrix[0].size() - 1; 
 
         while (rowBegin <= rowEnd && colBegin <= colEnd) {
-            // Traverse right
             for (int i = colBegin; i <= colEnd; i++) {
                 res.push_back(matrix[rowBegin][i]);
             }
             rowBegin++;
 
-            // Traverse down
             for (int i = rowBegin; i <= rowEnd; i++) {
                 res.push_back(matrix[i][colEnd]);
             }
             colEnd--;
 
-            // Traverse left
             if (rowBegin <= rowEnd) {
                 for (int i = colEnd; i >= colBegin; i--) {
                     res.push_back(matrix[rowEnd][i]);
@@ -32,7 +29,6 @@ public:
                 rowEnd--;
             }
 
-            // Traverse up
             if (colBegin <= colEnd) {
                 for (int i = rowEnd; i >= rowBegin; i--) {
                     res.push_back(matrix[i][colBegin]);
